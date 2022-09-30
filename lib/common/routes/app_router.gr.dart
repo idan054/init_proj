81b44b92a41fdd5310c2f01893cb3f886d6cd2d9
Bitons Mac <idanbit80@gmail.com>
2022-09-30 17:05:58 +0300

    Flutter init (V3) Google login included!: [X] Freezed [X] Models example [X] API example [X] Routes [X] Auth services [X] Extensions [X] Splash page [X] Navigation (Push) [ ] Themes (Dark / light) [X] Provider support

---
+++ lib/common/routes/app_router.gr.dart
@@ -0,0 +1,103 @@
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+//
+// ignore_for_file: type=lint
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:auto_route/auto_route.dart' as _i4;
+import 'package:example/common/splash_page.dart' as _i1;
+import 'package:example/screens/home/home_page.dart' as _i2;
+import 'package:example/screens/loginScreen.dart' as _i3;
+import 'package:flutter/material.dart' as _i5;
+
+class AppRouter extends _i4.RootStackRouter {
+  AppRouter([_i5.GlobalKey<_i5.NavigatorState>? navigatorKey])
+      : super(navigatorKey);
+
+  @override
+  final Map<String, _i4.PageFactory> pagesMap = {
+    SplashRoute.name: (routeData) {
+      return _i4.CustomPage<dynamic>(
+        routeData: routeData,
+        child: const _i1.SplashPage(),
+        opaque: true,
+        barrierDismissible: false,
+      );
+    },
+    HomeRoute.name: (routeData) {
+      return _i4.CustomPage<dynamic>(
+        routeData: routeData,
+        child: const _i2.HomePage(),
+        opaque: true,
+        barrierDismissible: false,
+      );
+    },
+    LoginScreen.name: (routeData) {
+      return _i4.CustomPage<dynamic>(
+        routeData: routeData,
+        child: const _i3.LoginScreen(),
+        opaque: true,
+        barrierDismissible: false,
+      );
+    },
+  };
+
+  @override
+  List<_i4.RouteConfig> get routes => [
+        _i4.RouteConfig(
+          SplashRoute.name,
+          path: '/',
+        ),
+        _i4.RouteConfig(
+          HomeRoute.name,
+          path: '/home-page',
+        ),
+        _i4.RouteConfig(
+          LoginScreen.name,
+          path: '/login-screen',
+        ),
+      ];
+}
+
+/// generated route for
+/// [_i1.SplashPage]
+class SplashRoute extends _i4.PageRouteInfo<void> {
+  const SplashRoute()
+      : super(
+          SplashRoute.name,
+          path: '/',
+        );
+
+  static const String name = 'SplashRoute';
+}
+
+/// generated route for
+/// [_i2.HomePage]
+class HomeRoute extends _i4.PageRouteInfo<void> {
+  const HomeRoute()
+      : super(
+          HomeRoute.name,
+          path: '/home-page',
+        );
+
+  static const String name = 'HomeRoute';
+}
+
+/// generated route for
+/// [_i3.LoginScreen]
+class LoginScreen extends _i4.PageRouteInfo<void> {
+  const LoginScreen()
+      : super(
+          LoginScreen.name,
+          path: '/login-screen',
+        );
+
+  static const String name = 'LoginScreen';
+}
